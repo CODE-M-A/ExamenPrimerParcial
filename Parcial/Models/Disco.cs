ee058a0e3224f2e7e56564e805b84e7b03fba0f7
Miguel <miguel.villca@uab.edu.bo>
2023-10-27 10:45:28 -0400

    Se agregaron capturas y el examen parcial

---
+++ Parcial/Models/Disco.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Disco
+    {
+        [Key]
+        public int IdDisco { get; set; }
+        public string Titulo { get; set; }
+        public string Autor { get; set; }
+        public int Año { get; set; }
+        
+
+       
+        
+    }
+}
---
+++ Parcial/Models/Musica.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class Musica
+    {
+        [Key]
+        public int IdMusica { get; set; }
+        public string Titulo { get; set; }
+        public string Genero { get; set; }
+        public int NumeroReproducciones { get; set; }
+        
+       
+        //public Universidad Universidad { get; set; }
+    }
+}
